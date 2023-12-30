5d0c60d51da3ee30b2bc646814414c8d1d7112cd
legowww <legowww@gmail.com>
2023-12-31 03:28:20 +0900

    git push v16

--- src/main/java/com/example/githubactionstest/ui/HealthcheckController.java
+++ src/main/java/com/example/githubactionstest/ui/HealthcheckController.java
@@ -1,14 +1,26 @@
 package com.example.githubactionstest.ui;
 
 
+import jakarta.annotation.PostConstruct;
+import org.apache.catalina.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HealthcheckController {
 
+    @Value("${message:defaultMessage}")
+    private String message;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("init: " + message);
+    }
+
     @GetMapping("/hk")
     public String healthCheck() {
-        return "SERVER IS ALIVE 12/24:18:00";
+        return "SERVER IS ALIVE: " + message;
     }
 }
